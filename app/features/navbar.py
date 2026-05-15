df43e6a52aaa7d00e73795cade1831e70a1bf8e2
Yuto Takano <moa17stock@gmail.com>
2026-05-15 03:14:54 +0200

    Implement setting WARN_EXPERIMENTAL at runtime, store build info

--- app/features/navbar.py
+++ app/features/navbar.py
@@ -1,11 +1,25 @@
 from dash import get_relative_path, html, dcc
 
+import os
+
+experimental = os.getenv("WARN_EXPERIMENTAL", "True") == "True"
+
 layout = (
     html.Header(
         [
-            html.H1(html.Div(id="navbar-home-click", children=dcc.Link("UN-ETH Policy Pulse", href=get_relative_path("/")))),
+            html.H1(
+                html.Div(
+                    id="navbar-home-click",
+                    children=[
+                        dcc.Link("UN-ETH Policy Pulse", href=get_relative_path("/")),
+                        html.Span(" Experimental Branch; Unstable!")
+                        if experimental
+                        else None,
+                    ],
+                )
+            ),
             html.Div(),
         ],
-        className="navbar",
+        className=f"navbar {'navbar-experimental' if experimental else ''}",
     ),
 )
